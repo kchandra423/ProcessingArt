4444152a1f85d081a4214d8d64191812302ffa7e
Kumar.s.Chandra <kchandra423@student.fuhsd.org>
2023-03-19 16:55:42 -0700

    boooo java is silly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,7 @@ public class Main {
         PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
         JFrame window = (JFrame)canvas.getFrame();
 
+
         window.setSize(600, 600);
         window.setMinimumSize(new Dimension(100,100));
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
